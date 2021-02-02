a2b50112218186cdc252efa1a2fce37216c90be9
Nikolai Yadrikhinskii <ya7kolya@gmail.com>
2021-02-02 19:12:53 +0900

    It now works, added comments

--- app/src/main/java/com/example/dialog_iadrikhinskii2/MainActivity.java
+++ app/src/main/java/com/example/dialog_iadrikhinskii2/MainActivity.java
@@ -9,14 +9,13 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Declaring variables
     private Button bgButton;
-    public ConstraintLayout relativeLayout;
+    public ConstraintLayout constraintLayout;
     Context context;
-    final CharSequence[] items = {getText(R.string.red), getText(R.string.yellow), getText(R.string.green)};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +24,44 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
 
         //Binding variables
         bgButton = (Button)findViewById(R.id.button_fon);
-        relativeLayout = (ConstraintLayout) findViewById(R.id.ConstraintLayout);
-        context = this;
+        constraintLayout = findViewById(R.id.ConstraintLayout);
+        context = MainActivity.this;
 
-        bgButton.setOnClickListener((View.OnClickListener) this);
-
+        //Add listener to button
+        bgButton.setOnClickListener(this);
     }
 
+    //When button is clicked
     @Override
-    public void onClick(DialogInterface dialog, int which) {
+    public void onClick(View v) {
+        //Strings for dialog
+        final CharSequence[] items = {getText(R.string.red), getText(R.string.green), getText(R.string.yellow)};
+
+        //Declaring builder for alert window, setting title for it, and adding options
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.message);
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 switch (which){
                     case 0:
-                        relativeLayout.setBackgroundResource(R.color.redColor);
+                        constraintLayout.setBackgroundResource(R.color.redColor);
+                        Toast.makeText(context, R.string.red, Toast.LENGTH_LONG).show();
                         break;
                     case 1:
-                        relativeLayout.setBackgroundResource(R.color.greenColor);
+                        constraintLayout.setBackgroundResource(R.color.greenColor);
+                        Toast.makeText(context, R.string.green, Toast.LENGTH_LONG).show();
                         break;
                     case 2:
-                        relativeLayout.setBackgroundResource(R.color.yellowColor);
+                        constraintLayout.setBackgroundResource(R.color.yellowColor);
+                        Toast.makeText(context, R.string.yellow, Toast.LENGTH_LONG).show();
                         break;
                 }
-                Toast.makeText(context, R.string.green, Toast.LENGTH_LONG).show();
             }
         });
+
+        //Creating AlertDialog from builder and showing it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
